06597ded51ddbb96716db42cc4a3e603c8c0719e
William Bach <williambach26@gmail.com>
2020-07-07 14:43:30 -0300

    + Como voce esta?

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -5,6 +5,7 @@ namespace Projeto_Teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está?");
         }
     }
 }
